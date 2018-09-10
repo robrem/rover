99a2f87889641570317bd5957e62680fbe40f269
Rob Remington <robrem@users.noreply.github.com>
2018-09-09 18:50:20 -0700

    respond to shake() by attempting to back up and turn right up to 3 times. Works occasionally, otherwise results in both motors firing 100% (error?)

--- code.py
+++ code.py
@@ -1,15 +1,16 @@
-import board
+import random
 import time
+from adafruit_circuitplayground.express import cpx
 from adafruit_crickit import crickit
-from digitalio import DigitalInOut, Direction, Pull
+
+LEFT = False
+RIGHT = True
 
 # Adjustment factors for motors at full speed
 RIGHT_SPEED = 1.0
 LEFT_SPEED = 0.9
 
-button_A = DigitalInOut(board.BUTTON_A)
-button_A.direction = Direction.INPUT
-button_A.pull = Pull.DOWN
+SHAKE_THRESHOLD = 10
  
 left_wheel = crickit.dc_motor_1
 right_wheel = crickit.dc_motor_2
@@ -24,6 +25,54 @@ def set_right(speed):
  
 def set_left(speed):
     left_wheel.throttle = -speed
+
+# React to accelerameter shake (bumped into something)
+# and try to move away.
+# Returns True if we're clear, False otherwise.
+def react_to_shake():
+    print("We are shaking!")
+    set_right(0.0)
+    set_left(0.0)
+    attempts = 0
+
+    while True:
+        if attempts == 3:
+            return False
+
+        if not cpx.shake(shake_threshold=SHAKE_THRESHOLD):
+            return True
+
+        # Back up
+        set_right(-0.5)
+        set_left(-0.5)
+        time.sleep(1.0)
+
+        # Try turning right
+        set_right(0.25)
+        set_left(LEFT_SPEED)
+
+        # Turn for this amount of time
+        time.sleep(random.choice([0.3, 0.4, 0.5]))
+        attempts += 1
+
+
+# Move in direction for duration. Returns True if move is 
+# successful, False otherwise.
+def tack(direction, duration):
+    target_time = time.monotonic() + duration
+
+    if direction == LEFT:
+        set_left(0.25)
+        set_right(RIGHT_SPEED)
+    else:
+        set_left(LEFT_SPEED)
+        set_right(0.25)
+
+    while time.monotonic() < target_time:
+        if cpx.shake(shake_threshold=SHAKE_THRESHOLD):
+            return react_to_shake()
+
+    return True
  
  
 # Uncomment this to find the above factors
@@ -33,14 +82,10 @@ def set_left(speed):
 #    pass
  
 while True:
-    if button_A.value:
-        while True:
-            # tack left
-            set_left(0.25)
-            set_right(RIGHT_SPEED)
-            time.sleep(0.75)
-         
-            # tack right
-            set_left(LEFT_SPEED)
-            set_right(0.25)
-            time.sleep(0.75)
+    if not tack(LEFT, 0.75):
+        break
+    if not tack(RIGHT, 0.75):
+        break
+
+set_right(0.0)
+set_left(0.0)
